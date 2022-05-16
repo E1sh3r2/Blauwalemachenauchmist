dc3faff85e1c1ef0f00c4f4228ff605dbde10706
Max Döge <mdoege@conet.de>
2022-05-16 16:05:35 +0200

    add thumbs for right and wrong

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 (() => {
     const STATES = { ANSWER: "answer", QUESTION: "question" }
     const $content = document.getElementById("content");
+    const $thumbs_group = document.getElementById("button_action");
+    const $thumbs_up = document.getElementById("thumbs_up");
+    const $thumbs_down = document.getElementById("thumbs_down");
 
     let currentState = STATES.QUESTION;
     let currentQuestion = "";
@@ -10,10 +13,20 @@
     }
 
     const displayQuestion = (question) => {
+        removeEventListenerThumbs()
+        addEventListenerContent()
+        $thumbs_group.style.display = "none"
+        $content.classList.remove("content-not-clickable")
+        $content.classList.add("content")
         $content.innerHTML = `<div id="question">${questions[question].question}</div>`
         currentState = STATES.ANSWER;
     }
     const displayAnswer = (question) => {
+        removeEventListenerContent()
+        addEventListenerThumbs()
+        $thumbs_group.style.display = "flex"
+        $content.classList.add("content-not-clickable")
+        $content.classList.remove("content")
         $content.innerHTML = `<div id="question">${questions[question].question}</div>` + questions[question].answer;
         currentState = STATES.QUESTION;
     }
@@ -27,13 +40,36 @@
         }
     }
 
+    const givenAnswer = () => {
+        /**
+         * Hier code für Cookies dies das
+         */
+        changeText()
+    }
+
+    const addEventListenerContent = () =>{
+        $content.addEventListener("click", changeText)
+    }
+
+    const removeEventListenerContent = () =>{
+        $content.removeEventListener("click", changeText)
+    }
+
+    const addEventListenerThumbs = () =>{
+        $thumbs_down.addEventListener("click", givenAnswer)
+        $thumbs_up.addEventListener("click", givenAnswer)
+    }
+
+    const removeEventListenerThumbs = () =>{
+        $thumbs_down.removeEventListener("click", givenAnswer)
+        $thumbs_up.removeEventListener("click", givenAnswer)
+    }
+
     document.addEventListener("keyup", (event) => {
         if (event.code === "Space") {
             changeText();
         }
     });
-
-    $content.addEventListener("click", changeText)
 
     selectRandomQuestion();
     displayQuestion(currentQuestion);
