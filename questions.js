a0f9690c76846070ff343563de0f82fb04eb2370
Max Döge <mdoege@conet.de>
2022-05-16 08:44:24 +0200

    update questions

---
+++ questions.js
@@ -0,0 +1,51 @@
+const questions = [
+  {
+    "question": "Definition Projekt",
+    "answer": "Einmaliges Vorhaben, Fester Start- und Endzeitpunkt, Begrenzte Ressourcen (Zeit, Personal, Budget), Gewisse Komplexität, Qualitätsanspruch"
+  },
+  {
+    "question": "Definition Magisches Dreieck (Projekt)",
+    "answer": "Leistung, Aufwand, Zeit - nicht alle 3 Faktoren können gleichzeitig erfüllt werden"
+  },
+  {
+    "question": "Beschreibe Software-Kriterien",
+    "answer": "Funktionalität - Umfang der Software\n"
+        + "Zuverlässigkeit - Identische Funktionalität - Über lange Zeiträume\n"
+        + "Benutzbarkeit - Usability - Software einfach zu nutzen - Intuitive Anwendung\n"
+        + "Effizienz - Software benötigt wenig Ressourcen - Hohe Lauffähigkeit und Arbeitsleistung\n"
+        + "Änderbarkeit - Modularer und klar strukturierte Software - Einfache Erweiterung der Software\n"
+        + "Übertragbarkeit - Übertragbarkeit auf andere Betriebssysteme - Responsive\n "
+        + "Portabilität\nRobustheit\nVerifizierbarkeit\nIntegrität"
+  },
+  {
+    "question": "Säulen der Objekt Orientierung nennen",
+    "answer": "Verkapselung:"
+        + "- Auf Methoden und Variablen können unterschiedlich zugegriffen werden"
+        + "- Private – nur innerhalb in der Klasse, nicht vererbbar"
+        + "- Public – außerhalb der Klasse sowie Unterklassen"
+        + "- Package – innerhalb des gleichen Pakets"
+        + "- Protected – für Klassen und Unterklassen (vererbte Klassen) sichtbar"
+        + "Vererbung:"
+        + "- Unterklassen erben Methoden und Verhalten von Oberklassen"
+        + "- Unterklassen können Methoden von der Oberklasse überschreiben"
+        + "- Generalisierung, Spezialisierung"
+        + "Polymorphie (Vielgestaltigkeit):"
+        + "- Poly = viele, morph = Form"
+        + "- Implementierung von Oberklassen Methoden"
+        + "- Methoden können überschrieben werden"
+        + "Abstraktion:"
+        + "- Die anderen Säulen sind eine Form der Abstraktion"
+        + "- Bei der Modellierung werden bewusst bestimmte Eigenschaften „verborgen“"
+        + "- Nur notwendige Eigenschaften bleiben behalten"
+},{
+    "question": "Was ist ein Objekt?",
+    "answer": "Objekt"
+        + "- Instanz von einer Klasse („new …“)"
+        + "- Über Attribute eindeutig beschreibbar"
+        + "- Verfügen über eine Lebensdauer"
+},{
+    "question": "Was ist eine Klasse?",
+    "answer": "Klasse"
+        + "-  Bauplan für ein Objekt"
+}
+]
