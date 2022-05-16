256d3297681450f8383680500211f9c51cc779c8
Henri <ie19a.matthiessen.henri@bk-hennef.de>
2022-05-16 09:47:37 +0200

    Mobile Support

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const questions = [{"question": "TestQuestion1", "answer": "TestAnswer12"}]
 let state = "Question"
 let question = ""
 
 function displayQuestion() {
     question = Math.floor(Math.random() * questions.length)
-    document.getElementById("text").innerHTML = questions[question].question
+    document.getElementById("content").innerHTML = questions[question].question
     state = "Answer"
 }
 
 function displayAnswer() {
-    document.getElementById("text").innerHTML = questions[question].answer
+    document.getElementById("content").innerHTML = questions[question].answer
     state = "Question"
 }
 
@@ -20,5 +19,11 @@ function changeText() {
         displayAnswer()
     }
 }
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+        changeText()
+    }
+})
 
 displayQuestion()
