a195ccc4b8816a60dc623422e39484f0c4c30145
Max Döge <mdoege@conet.de>
2022-05-23 15:30:02 +0200

    add question id to url

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,102 +1,151 @@
 (() => {
-    const STATES = { ANSWER: "answer", QUESTION: "question" };
-    const ANSWER = { RIGHT: "right", WRONG: "wrong" };
-    const COOKIE_KEY_STATS = "blauwale_stats";
-    const $currentText = document.getElementById("currentText");
-    const $decisionButtons = document.getElementById("decisionButtons");
-    const $answerWrong = document.getElementById("answerWrong");
-    const $answerRight = document.getElementById("answerRight");
+  const STATES = {ANSWER: "answer", QUESTION: "question"};
+  const ANSWER = {RIGHT: "right", WRONG: "wrong"};
+  const URL_PARAMETER_QUESTION = "question"
+  const COOKIE_KEY_STATS = "blauwale_stats";
+  const $currentText = document.getElementById("currentText");
+  const $decisionButtons = document.getElementById("decisionButtons");
+  const $answerWrong = document.getElementById("answerWrong");
+  const $answerRight = document.getElementById("answerRight");
 
-    const mdParser = new markdownit();
+  const mdParser = new markdownit();
 
-    let currentState = STATES.QUESTION;
-    let currentQuestion = "";
+  let currentState = STATES.QUESTION;
+  let currentQuestion = "";
 
-    const selectRandomQuestion = () => {
-        currentQuestion = Math.floor(Math.random() * questions.length);
-    };
+  const selectRandomQuestion = () => {
+    currentQuestion = Math.floor(Math.random() * questions.length);
+  };
 
-    const displayQuestion = () => {
-        $decisionButtons.style.display = "none";
-        $currentText.innerHTML = questions[currentQuestion].question;
-        $currentText.classList.toggle("question");
-        currentState = STATES.QUESTION;
-    };
+  const displayQuestion = () => {
+    $decisionButtons.style.display = "none";
+    $currentText.innerHTML = questions[currentQuestion].question;
+    $currentText.classList.toggle("question");
+    currentState = STATES.QUESTION;
+  };
 
-    const displayAnswer = () => {
-        $decisionButtons.style.display = "flex";
-        $currentText.innerHTML =
-            questions[currentQuestion].question +
-            mdParser.render(questions[currentQuestion].answer);
-        $currentText.classList.toggle("question");
-        currentState = STATES.ANSWER;
-    };
+  const displayAnswer = () => {
+    $decisionButtons.style.display = "flex";
+    $currentText.innerHTML =
+        questions[currentQuestion].question +
+        mdParser.render(questions[currentQuestion].answer);
+    $currentText.classList.toggle("question");
+    currentState = STATES.ANSWER;
+  };
 
-    /**
-     * Retrieves all answers as questionId-decision map from local storage
-     * @returns answers from local storage. Returns empty object if local storage are not available
-     */
-    const retrieveStatsFromLocalStorage = () => {
-        if ("localStorage" in window) {
-            const statsStr = window.localStorage.getItem(COOKIE_KEY_STATS);
+  /**
+   * Retrieves all answers as questionId-decision map from local storage
+   * @returns answers from local storage. Returns empty object if local storage are not available
+   */
+  const retrieveStatsFromLocalStorage = () => {
+    if ("localStorage" in window) {
+      const statsStr = window.localStorage.getItem(COOKIE_KEY_STATS);
 
-            if (statsStr) {
-                const stats = JSON.parse(statsStr);
-                return stats;
-            }
-        } else console.warning("local storage is not available");
+      if (statsStr) {
+        const stats = JSON.parse(statsStr);
+        return stats;
+      }
+    } else {
+      console.warning("local storage is not available");
+    }
 
-        return { answers: {} };
-    };
+    return {answers: {}};
+  };
 
-    /**
-     * Save answer of question to local storage
-     * @param {number} id question id
-     * @param {ANSWER} decision answer to question
-     */
-    const saveAnswerToLocalStorage = (id, decision) => {
-        const stats = retrieveStatsFromLocalStorage();
+  /**
+   * Save answer of question to local storage
+   * @param {number} id question id
+   * @param {ANSWER} decision answer to question
+   */
+  const saveAnswerToLocalStorage = (id, decision) => {
+    const stats = retrieveStatsFromLocalStorage();
 
-        // increment count of answer
-        if (typeof stats["answers"][id] !== "undefined") {
-            stats["answers"][id][decision].count = stats.count + 1;
-        } else {
-            stats["answers"][id] = {
-                [ANSWER.RIGHT]: decision === ANSWER.RIGHT ? 1 : 0,
-                [ANSWER.WRONG]: decision === ANSWER.WRONG ? 1 : 0,
-            };
-        }
+    // increment count of answer
+    if (typeof stats["answers"][id] !== "undefined") {
+      stats["answers"][id][decision].count = stats.count + 1;
+    } else {
+      stats["answers"][id] = {
+        [ANSWER.RIGHT]: decision === ANSWER.RIGHT ? 1 : 0,
+        [ANSWER.WRONG]: decision === ANSWER.WRONG ? 1 : 0,
+      };
+    }
 
-        if ("localStorage" in window) {
-            window.localStorage.setItem(
-                COOKIE_KEY_STATS,
-                JSON.stringify(stats)
-            );
-        } else console.warning("local storage is not available");
-    };
+    if ("localStorage" in window) {
+      window.localStorage.setItem(
+          COOKIE_KEY_STATS,
+          JSON.stringify(stats)
+      );
+    } else {
+      console.warning("local storage is not available");
+    }
+  };
 
-    const selectAnswer = (answer) => () => {
-        saveAnswerToLocalStorage(currentQuestion, answer);
-        selectRandomQuestion();
-        displayQuestion();
-    };
-
-    /**
-     * On click event handler for content div
-     * @param {Event} event default click event
-     */
-    const onContentClicked = (event) => {
-        if (currentState === STATES.ANSWER) {
-            event.preventDefault();
-        } else {
-            displayAnswer();
-        }
-    };
-
-    $currentText.addEventListener("click", onContentClicked);
-    $answerWrong.addEventListener("click", selectAnswer(ANSWER.WRONG));
-    $answerRight.addEventListener("click", selectAnswer(ANSWER.RIGHT));
-
+  const selectAnswer = (answer) => () => {
+    saveAnswerToLocalStorage(currentQuestion, answer);
     selectRandomQuestion();
-    displayQuestion();
+    setUrlParameter(URL_PARAMETER_QUESTION, currentQuestion)
+  };
+
+  /**
+   * On click event handler for content div
+   * @param {Event} event default click event
+   */
+  const onContentClicked = (event) => {
+    if (currentState === STATES.ANSWER) {
+      event.preventDefault();
+    } else {
+      displayAnswer();
+    }
+  };
+
+  /**
+   * get one url parameter
+   * @param param parameter
+   * @returns {string} value of parameter
+   */
+  const getUrlParameter = (param) => {
+    const sPageURL = window.location.search.substring(1);
+    const sURLVariables = sPageURL.split('&');
+    for (let i = 0; i < sURLVariables.length; i++) {
+      const sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] === param) {
+        return sParameterName[1];
+      }
+    }
+  };
+
+  /**
+   * set current parameter to url
+   * @param param name of parameter
+   * @param value new value of parameter
+   */
+  const setUrlParameter = (param, value) => {
+    if ('URLSearchParams' in window) {
+      var searchParams = new URLSearchParams(window.location.search);
+      searchParams.set(param, value);
+      window.location.search = searchParams.toString();
+    }
+  };
+
+  /**
+   * check parameter is number
+   * @param param parameter
+   * @returns {number} parameter as number or NaN
+   */
+  const isParamNumber = (param) => {
+    return parseInt(param)
+  }
+
+  $currentText.addEventListener("click", onContentClicked);
+  $answerWrong.addEventListener("click", selectAnswer(ANSWER.WRONG));
+  $answerRight.addEventListener("click", selectAnswer(ANSWER.RIGHT));
+
+  let param = isParamNumber(getUrlParameter(URL_PARAMETER_QUESTION))
+  if (!isNaN(param)) {
+    currentQuestion = param
+  } else {
+    selectRandomQuestion();
+  }
+  displayQuestion();
+
 })();
